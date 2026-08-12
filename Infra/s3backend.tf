2b20c006f0d001568b58f25f7738ca2b2fdafee8
moyusuf44 <mohamedyusuf.6445@gmail.com>
2026-08-12 18:28:04 +0300

    region added

--- Infra/s3backend.tf
+++ Infra/s3backend.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
-        bucket         = ""
-        key            = ""
-        region         = var.region_id
+        bucket         = "moyusufs-aiapp-terraform-state"
+        key            = "ai-app/terraform.tfstate"
+        region         = "eu-north-1"
         dynamodb_table = "terraform-aiapp-state-lock"
         encrypt        = true 
     }
