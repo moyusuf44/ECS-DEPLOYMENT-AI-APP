e9109827ce2276e56f76ba7680aba11d873abdb0
moyusuf44 <mohamedyusuf.6445@gmail.com>
2026-08-13 15:37:57 +0300

    resource

--- Infra/modules/03-ecs/main.tf
+++ Infra/modules/03-ecs/main.tf
@@ -92,3 +92,21 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
     role       = aws_iam_role.ecs_task_execution_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy" "secrets_manager" {
+  name = "ecs-secrets-manager"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = aws_secretsmanager_secret.openai_api_key.arn
+      }
+    ]
+  })
+}
